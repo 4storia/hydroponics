9d0c282a27ef4a2b4ca70669532eab65f1f5c04a
Cliff Hess <cwhess@ncsu.edu>
2022-08-21 10:01:40 -0400

    Adding water pump management code for watering 3x a day on an 8hr interval

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const PowerStrip = require('./tp-link/power-strip');
 
 const POWER_STRIP_HOST = '192.168.50.245';
-const UV_LIGHTING_PLUG = 1;
+const MAIN_PLANTS_UV_LIGHT_PLUG = 1;
+const SEEDLINGS_UV_LIGHT_PLUG = 2;
+const WATER_PUMP_PLUG = 3;
 
 const FIFTEEN_MINUTES = 1000 * 60 * 15;
+const WATERING_HOURS = [9, 17, 1];
 
 async function timeLoop() {
     await controlPower();
@@ -13,20 +16,52 @@ async function controlPower() {
     const powerStrip = new PowerStrip(POWER_STRIP_HOST);
     await powerStrip.connect();
 
+    manageLights(powerStrip);
+    manageWater(powerStrip);
+}
+
+function manageLights(powerStrip) {
     const now = new Date();
     const currentHour = now.getHours();
     const dayTime = currentHour >= 9 && currentHour < 23;
 
     const formattedDate = `${now.toLocaleDateString('en-US')} ${now.toLocaleTimeString('en-US')}`;
 
-    if(dayTime && powerStrip.getPowerStatusForPlug(UV_LIGHTING_PLUG) === 0) {
-        console.log(`[ ${formattedDate} ]: Turning UV Lighting ON`);
-        powerStrip.setPowerForPlug(UV_LIGHTING_PLUG, 1)
+    // Main UV Light
+    if(dayTime && powerStrip.getPowerStatusForPlug(MAIN_PLANTS_UV_LIGHT_PLUG) === 0) {
+        console.log(`[ ${formattedDate} ]: ON - Main UV lighting`);
+        powerStrip.setPowerForPlug(MAIN_PLANTS_UV_LIGHT_PLUG, 1)
+    } else if(!dayTime && powerStrip.getPowerStatusForPlug(MAIN_PLANTS_UV_LIGHT_PLUG) === 1) {
+        console.log(`[ ${formattedDate} ]: OFF - Main UV lighting`);
+        powerStrip.setPowerForPlug(MAIN_PLANTS_UV_LIGHT_PLUG, 0)
     }
 
-    if(!dayTime && powerStrip.getPowerStatusForPlug(UV_LIGHTING_PLUG) === 1) {
-        console.log(`[ ${formattedDate} ]: Turning UV Lighting OFF`);
-        powerStrip.setPowerForPlug(UV_LIGHTING_PLUG, 0)
+    // Seedling light
+    if(dayTime && powerStrip.getPowerStatusForPlug(SEEDLINGS_UV_LIGHT_PLUG) === 0) {
+        console.log(`[ ${formattedDate} ]: ON - Seedling UV lighting`);
+        powerStrip.setPowerForPlug(SEEDLINGS_UV_LIGHT_PLUG, 1)
+    } else if(!dayTime && powerStrip.getPowerStatusForPlug(SEEDLINGS_UV_LIGHT_PLUG) === 1) {
+        console.log(`[ ${formattedDate} ]: OFF - Seedling UV lighting`);
+        powerStrip.setPowerForPlug(SEEDLINGS_UV_LIGHT_PLUG, 0)
+    }
+}
+
+function manageWater(powerStrip) {
+    const now = new Date();
+    const currentHour = now.getHours();
+    const currentMinutes = now.getMinutes();
+    const formattedDate = `${now.toLocaleDateString('en-US')} ${now.toLocaleTimeString('en-US')}`;
+
+    const isWateringHour = WATERING_HOURS.includes(currentHour);
+    const shouldTurnWaterOn = isWateringHour && currentMinutes < 15;
+    const isWaterAlreadyOn = powerStrip.getPowerStatusForPlug(WATER_PUMP_PLUG) === 1;
+
+    if(shouldTurnWaterOn && !isWaterAlreadyOn) {
+        console.log(`[ ${formattedDate} ]: ON - Water pump`);
+        powerStrip.setPowerForPlug(WATER_PUMP_PLUG, 1)
+    } else if(!shouldTurnWaterOn && isWaterAlreadyOn) {
+        console.log(`[ ${formattedDate} ]: OFF - Water pump`);
+        powerStrip.setPowerForPlug(WATER_PUMP_PLUG, 0)
     }
 }
 
